4c0f94f0a3b1c68287dae740a1eb9c4e249ffada
Ernestas <drplauska@gmail.com>
2020-10-26 20:34:34 +0200

    add baseground before eject so I can use tailwind and absolute paths

---
+++ src/components/StationsList/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StationsList() {
+  return <div>Text</div>;
+}
+
+export default StationsList;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as StationsList } from "./StationsList";
