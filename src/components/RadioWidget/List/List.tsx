427fc0fee295e34f01ea47326883e1bdc166535c
Ernestas <drplauska@gmail.com>
2020-10-27 23:16:05 +0200

    add jest tests

--- src/components/RadioWidget/List/List.tsx
+++ src/components/RadioWidget/List/List.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import MinusIcon from '@assets/icons/minus.png';
 import PlusIcon from '@assets/icons/plus.png';
-import StationAvatar from '@assets/images/station_avatar.png';
+import StationAvatar from '@assets/images/station-avatar.png';
 import { Station } from '../types';
 import styles from './index.module.scss';
 
@@ -9,24 +9,36 @@ interface ListProps {
   onClick: (station: Station | null) => void;
   data: Station[];
   activeStationId: String | undefined;
+  onVolumeIncrement: () => void;
+  onVolumeDecrement: () => void;
 }
 
-function List({ data, onClick, activeStationId }: ListProps) {
+function List({ data, onClick, activeStationId, onVolumeIncrement, onVolumeDecrement }: ListProps) {
+  const renderControls = useCallback(
+    (id: string) => {
+      if (id === activeStationId) {
+        return (
+          <div className={styles.detail}>
+            <button type="button" onClick={onVolumeDecrement}>
+              <img src={MinusIcon} alt="minus" className={styles.button} />
+            </button>
+            <img src={StationAvatar} alt="station avatar" className={styles.avatar} />
+            <button type="button" onClick={onVolumeIncrement}>
+              <img src={PlusIcon} alt="plus" className={styles.button} />
+            </button>
+          </div>
+        );
+      }
+      return null;
+    },
+    [activeStationId]
+  );
+
   return (
     <div className={styles.list}>
       {data.map((item) => (
         <div key={item.id} className={styles.item}>
-          {activeStationId === item.id && (
-            <div className={styles.detail}>
-              <button type="button">
-                <img src={MinusIcon} alt="minus" className={styles.button} />
-              </button>
-              <img src={StationAvatar} alt="station avatar" className={styles.avatar} />
-              <button type="button">
-                <img src={PlusIcon} alt="plus" className={styles.button} />
-              </button>
-            </div>
-          )}
+          {renderControls(item.id)}
           <button
             type="button"
             className={styles.details}
